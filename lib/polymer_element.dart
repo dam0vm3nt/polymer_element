59ba25b6d0e554d8bf51e725234485669646c55b
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-10-03 00:26:03 +0200

    polymerize better

--- lib/polymer_element.dart
+++ lib/polymer_element.dart
@@ -1,12 +1,7 @@
-@JS('Polymer')
 library polymer_element;
 
 import 'dart:html' as html;
-import 'dart:html_common';
 
-import 'package:js/js.dart';
-
-//@JS('Element')
 abstract class Element implements html.Element  {
   set(propertyName,value);
 }
