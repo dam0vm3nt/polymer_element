a53ce4964f1390164d080a3a77ad20b7418f1741
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-10-05 11:51:38 +0200

    lets do some transofmer

--- lib/polymer_element.dart
+++ lib/polymer_element.dart
@@ -1,10 +1,29 @@
 library polymer_element;
 
 import 'dart:html' as html;
-import 'dart:js_util' show callMethod,getProperty;
+import 'dart:js';
 
-class PolymerElement extends html.HtmlElement  {
-  PolymerElement() : super.created();
+class Config {
+  List observers;
 
-  
+  Config({this.observers});
+}
+
+class PolymerElement extends html.HtmlElement {
+  PolymerElement() : super.created() {
+    _jsObject = new JsObject.fromBrowserObject(this);
+  }
+
+  JsObject _jsObject;
+  JsObject _callSuper(String name, [List args = const []]) =>
+      _jsObject.callMethod('__callSuper', [name, args]);
+
+  connectedCallback() {
+    _callSuper('connectedCallback');
+  }
+
+  // Private method can be overridden safely because they become Symbol ...
+  //_invalidateProperties() => _jsObject.callMethod('_invalidateProperties', []);
+
+  set(name, val) => _callSuper('set', [name, val]);
 }
