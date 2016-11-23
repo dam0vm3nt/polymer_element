cd490b3259f7687bf5a563a31694f5ed1629b0c8
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-11-23 15:14:14 +0100

    better native support

---
+++ lib/iron_input.dart
@@ -0,0 +1,11 @@
+@JS('PolymerElements')
+library iron_input;
+import 'dart:html';
+import 'package:js/js.dart';
+import 'package:polymer_element/polymer_element.dart';
+
+
+//@JS('PaperInput')
+@PolymerRegister('iron-input',template:'src/iron-input/iron-input.html',native:true)
+class IronInput extends PolymerElement {
+}
--- lib/paper_input.dart
+++ lib/paper_input.dart
@@ -2,12 +2,13 @@
 library paper_input;
 import 'dart:html';
 import 'package:js/js.dart';
+import 'package:polymer_element/iron_input.dart';
 import 'package:polymer_element/polymer_element.dart';
 
 
 //@JS('PaperInput')
 @PolymerRegister('paper-input',template:'src/paper-input/paper-input.html',native:true)
 class PaperInput extends PolymerElement {
-  external InputElement get inputElement;
+  external IronInput get inputElement;
 
 }
