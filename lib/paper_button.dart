b3a7202aeeb8b5ac2fdb8afab438d69473898e50
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-12-09 17:17:27 +0100

    cips

--- lib/paper_button.dart
+++ lib/paper_button.dart
@@ -2,13 +2,16 @@
 library paper_button;
 import 'dart:html';
 import 'package:js/js.dart';
-import 'package:polymer_element/iron_input.dart';
 import 'package:polymer_element/polymer_element.dart';
 
 
+abstract class PaperButtonBehavior {
+  bool get raised;
+  set raised(bool value);
+}
+
 //@JS('PaperButton')
 @PolymerRegister('paper-button',template:'src/paper-button/paper-button.html',native:true)
-class PaperButton extends PolymerElement {
-  external IronInput get inputElement;
+class PaperButton extends PolymerElement with PaperButtonBehavior {
 
 }
--- lib/paper_dialog.dart
+++ lib/paper_dialog.dart
@@ -5,9 +5,11 @@ import 'package:polymer_element/polymer_element.dart';
 
 
 //@JS('PaperDialog')
+
+
 @PolymerRegister('paper-dialog',template:'src/paper-dialog/paper-dialog.html',native:true)
-class PaperDialog extends PolymerElement {
-  external bool get opened;
-  external set opened(bool v);
-  external void open();
+abstract class PaperDialog extends PolymerElement {
+  bool get opened;
+  set opened(bool v);
+  void open();
 }
