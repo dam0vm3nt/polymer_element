1cd652fbcb33c9ad99b77a336d36c417184cf269
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-12-12 16:03:07 +0100

    update to latest version, some fix for `require`

--- lib/require.dart
+++ lib/require.dart
@@ -5,18 +5,31 @@ import 'dart:async';
 import 'dart:js';
 
 import 'package:js/js.dart';
-import 'package:js/js_util.dart';
+import 'package:js/js_util.dart' show callMethod;
+import 'dart:html';
 
 @JS('require')
-external _require(List modules,Function body);
+external _require(List modules, Function body);
 
-Future<List> require(List<String> modules) {
+Future<List> require(List<String> modules) async {
+  // Load all html imports
+  await Future.wait(modules.map((module) {
+    LinkElement lnk = new LinkElement()
+      ..rel = 'import'
+      ..href = "${module}.html";
+    document.head.append(lnk);
+    return lnk.onLoad.first;
+  }));
+
   Completer<List> whenLoaded = new Completer();
 
-  _require(['external/polymer_element/utils'],(utils) {
-    callMethod(utils,'require_varargs',[modules,(res) {
+  _require(['external/polymer_element/utils'], (utils) {
+    callMethod(utils, 'require_varargs', [
+      modules,
+      (res) {
         whenLoaded.complete(res);
-    }]);
+      }
+    ]);
   });
 /*
   context.callMethod('require', [
