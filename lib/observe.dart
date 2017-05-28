1a7c08617172e829941cb0b64d4b1a716ffcbbc3
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2017-05-28 18:03:10 +0200

    more performant observer

--- lib/observe.dart
+++ lib/observe.dart
@@ -16,9 +16,19 @@ import 'package:polymerize_common/html_import.dart';
 @JS()
 @anonymous
 class _Support {
-  external makeObservable(obj, observer callback);
-  external cancelObserver(obj, observer callback);
+  external makeObservable(obj, observer callback, callbackFactory factory);
+  external cancelObserver(obj, observer callback, callbackFactory factory);
   external List<String> findProps(obj);
+}
+
+typedef CallbackFactoryResult callbackFactory(String property, value);
+
+@JS()
+@anonymous
+class CallbackFactoryResult {
+  external observer get callback;
+  external callbackFactory get factory;
+  external factory CallbackFactoryResult({observer callback, callbackFactory factory});
 }
 
 typedef observer(String propertyName, oldValue, newValue);
@@ -44,9 +54,9 @@ class ObserveSupport {
    * Calling it on an already observable object returns the same object but
    * adds a listener.
    */
-  X makeObservable<X>(X obj, [observer callback]) => _observe_support.makeObservable(obj, callback);
+  X makeObservable<X>(X obj, {observer callback, callbackFactory factory}) => _observe_support.makeObservable(obj, callback, factory);
 
-  cancelObserver(obj, observer callback) => _observe_support.cancelObserver(obj, callback);
+  cancelObserver(obj, observer callback, [callbackFactory factory]) => _observe_support.cancelObserver(obj, callback, factory);
 
   List<String> findProps(obj) => _observe_support.findProps(obj);
 }
@@ -73,27 +83,21 @@ class _JSObject {
 abstract class AutonotifyBehavior implements DartCallbacksBehavior, PropertyObserverBehavior {
   //PropertyChangeHandler _rootHandler;
   Set<String> _topLevelProps;
-  Observer _mainObserver;
   Notifier _mainNotifier;
   void readyPreHook() {
-    String tagName = (this as HTMLElement).tagName;
-    Metadata data = metadataRegistry[tagName];
-
-    // Need to observe only the top level names of nested props
-    //_rootHandler = new PropertyChangeHandler.root(this as PolymerElement);
-    _topLevelProps = new Set.from(data.observedPaths.map((p) => p.split('.').first));
-    _mainObserver = new Observer();
-    _mainNotifier = new Notifier(this, "", (prop) => onNotify(prop), _mainObserver, filter: _topLevelProps);
-
+    _mainNotifier = new Notifier(this, "", (prop) => onNotify(prop));
   }
 
   void onPropertiesChangedPreHook(data, changedProps, oldData) {
-
     JSObject.keys(changedProps).forEach((propName) {
       var oldv = getProperty(oldData, propName);
       var newv = getProperty(data, propName);
+      var pxy = observeSupport.makeObservable(newv);
+      if (newv!=pxy) {
+        new Future(() => setProperty(data, propName, pxy)); // schedule replace
+      }
       if (oldv != newv) {
-        _mainObserver(propName, oldv, newv);
+        _mainNotifier.onChange(propName, oldv, newv);
       }
     });
   }
@@ -107,73 +111,24 @@ abstract class AutonotifyBehavior implements DartCallbacksBehavior, PropertyObse
 
 typedef void Notify(String path);
 
-class Change {
-  String property;
-  var oldValue;
-  var newValue;
-
-  Change(this.property, this.oldValue, this.newValue);
-}
-
-class Observer {
-  StreamController<Change> _consumer = new StreamController.broadcast();
-  Stream<Change> get changes => _consumer.stream;
-
-  void call(propName, oldv, newv) {
-    _consumer.add(new Change(propName, oldv, newv));
-  }
-}
-
 class Notifier {
   String _path;
   Notify _notify;
-  Observer _observer;
   var _obj;
   Map<String, Notifier> _childObservers = {};
+  observer _observer;
 
-  Notifier._(this._obj, this._path, this._notify, this._observer) {}
-
-  factory Notifier(_obj, String _path, Notify _notify, Observer _observer, {Set<String> filter,Set examined}) {
-    // TODO : CONVERTIRE IN FACTORY ED EVITARE I LOOP RICORSIVI
-    if (examined == null) {
-      examined = new Set();
-    }
-
-    if (examined.contains(_obj)) {
-      return null;
-    }
-
-    examined.add(_obj);
-
-    Notifier n = new Notifier._(_obj, _path, _notify, _observer);
-    List<String> props = observeSupport.findProps(_obj);
-    print("FOUND ${props} in ${_obj}");
-    props.forEach((name) {
-      if (filter != null && !filter.contains(name)) return;
-      var val = getProperty(_obj, name);
-      if (val != null) {
-        n._observeProp(val, name,examined);
-      }
-    });
-
-    examined.remove(_obj);
-
-    // Add listeners
-    _observer.changes.listen(n._onChange);
-
-    return n;
+  Notifier(this._obj, this._path, this._notify) {
+    _observer = (String propName, oldv, newv) {
+      onChange(propName, oldv, newv);
+    };
   }
 
-  void _observeProp(val, String name,[Set examined]) {
-    Observer obs = new Observer();
-    var obsVal = observeSupport.makeObservable(val, obs);
-    if (obsVal != null) {
-      Notifier forProp = new Notifier(val, "${_path}${name}.", _notify, obs,examined: examined);
-      if (obsVal!=val) {
-        setProperty(_obj, name, obsVal);
-      }
-      _childObservers[name] = forProp;
-    }
+  CallbackFactoryResult call(String property, value) {
+    Notifier notifier = new Notifier(value, "${_path}${property}.", _notify);
+    _childObservers[property] = notifier;
+
+    return new CallbackFactoryResult(callback: notifier._observer, factory: notifier);
   }
 
   void close() {
@@ -182,28 +137,32 @@ class Notifier {
     });
     _childObservers.clear();
     // Stop observing
-    observeSupport.cancelObserver(_obj, _observer);
+    observeSupport.cancelObserver(observeSupport.makeObservable(_obj), _observer, this);
 
     // clear refs
-    _observer = null;
+
     _obj = null;
     _notify = null;
     _path = null;
   }
 
-  void _onChange(Change chg) {
+  void onChange(String propName, oldv, newv) {
     // Remove prev listener
-    Notifier _prev = _childObservers.remove(chg.property);
+    Notifier _prev = _childObservers.remove(propName);
     if (_prev != null) {
       _prev.close();
     }
 
-    if (chg.newValue != null) {
-      _observeProp(chg.newValue, chg.property);
+    if (newv != null) {
+      CallbackFactoryResult res = this(propName, newv);
+      var pxy= observeSupport.makeObservable(newv, callback: res.callback, factory: res.factory);
+      if (pxy!=newv && pxy!=null) {
+        new Future(()=>setProperty(_obj, propName, pxy));
+      }
     }
 
     // Notify
-    _notify("${_path}${chg.property}");
+    _notify("${_path}${propName}");
 
     // If list notify length too
     // (investigate on why it doesn't get notified automatically)
@@ -211,111 +170,4 @@ class Notifier {
       _notify("${_path}length");
     }
   }
-}
-
-class PropertyChangeHandler {
-  PolymerElement _target;
-  String _propPath;
-  String _propName;
-  bool _notify = true;
-  Map<String, PropertyChangeHandler> _subPropertyHandlers;
-
-  PropertyChangeHandler();
-  var parent;
-  var current;
-
-  void call(prop, oldv, newv) {
-    PropertyChangeHandler handler = _subPropertyHandlers[prop];
-    if (handler != null) {
-      handler.handlePropertyChange(current, oldv, newv);
-    }
-  }
-
-  void install(parent) {
-    this.parent = parent;
-    if (_propName != null && _subPropertyHandlers.isNotEmpty) {
-      current = getProperty(parent, _propName);
-
-      if (current != null) {
-        setProperty(parent, _propName, observeSupport.makeObservable(current, this));
-      }
-
-      // install subhandlers too
-      installSubListeners();
-    }
-  }
-
-  void installSubListeners() {
-    if (current != null) {
-      _subPropertyHandlers.forEach((pname, handler) {
-        handler.install(current);
-      });
-    }
-  }
-
-  void uninstallSubListeners() {
-    _subPropertyHandlers.values.forEach((sub) => sub.uninstall());
-  }
-
-  void uninstall() {
-    uninstallSubListeners();
-    if (current != null) {
-      observeSupport.cancelObserver(current, this);
-      current = null;
-    }
-  }
-
-  factory PropertyChangeHandler.root(PolymerElement target) {
-    Metadata data = metadataRegistry[target.tagName];
-    if (data == null) {
-      return null;
-    }
-
-    PropertyChangeHandler handler = new PropertyChangeHandler()
-      .._target = target
-      .._propPath = null
-      .._propName = null
-      .._notify = false
-      .._subPropertyHandlers = createHandlerTree(target, data.observedPaths.map((x) => x.split('.')), notify: false);
-
-    return handler;
-  }
-
-  static Map<String, PropertyChangeHandler> createHandlerTree(PolymerElement target, Iterable<List<String>> propPaths, {String rootPath = "", bool notify: true}) {
-    Map<String, List<List<String>>> tree = {};
-
-    propPaths.where((l) => l.isNotEmpty).forEach((List<String> propPath) {
-      String topLevelName = propPath.first;
-      List<List<String>> subTree = tree.putIfAbsent(topLevelName, () => []);
-      subTree.add(propPath.sublist(1));
-    });
-
-    Map<String, PropertyChangeHandler> handlers = {};
-    // Now create handlers
-    tree.forEach((String topLevelName, List<List<String>> subPaths) {
-      String thisPath = "${rootPath}${topLevelName}";
-      handlers[topLevelName] = new PropertyChangeHandler()
-        .._target = target
-        .._propPath = thisPath
-        .._propName = topLevelName
-        .._notify = notify
-        .._subPropertyHandlers = createHandlerTree(target, subPaths, rootPath: "${thisPath}.");
-    });
-
-    return handlers;
-  }
-
-  void handlePropertyChange(parent, oldv, newv) {
-    if (_notify) _target.notifyPath(_propPath);
-
-    // uninstall old sub listeners
-    uninstall();
-
-    current = newv;
-
-    // install new sub listeners
-    install(parent);
-  }
-
-  toString() => "{ ${_propPath} -> [ ${_subPropertyHandlers.values.map((x) => x.toString()).join(',\n')} ] }";
 }
