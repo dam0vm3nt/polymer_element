03b5b16664b10ae938d91959ccdc88008e4156db
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2017-06-03 10:10:23 +0200

    using export as well as imports to discover dependencies

---
+++ lib/annotations.dart
@@ -0,0 +1,63 @@
+
+class PolymerRegister {
+  final String tagName;
+  final String template;
+  final bool native;
+  final List<Type> uses;
+  const PolymerRegister(this.tagName,
+                        {this.template, this.native: false, this.uses});
+}
+
+class BowerImport {
+  final String ref;
+  final String import;
+  final String name;
+  const BowerImport({this.ref, this.import, this.name});
+}
+
+
+/**
+ * Optional property annotation in order to add metadata info.
+ */
+class Property {
+  final bool notify;
+  /// TODO: not yet implemented
+  final String computed;
+  final String statePath;
+  /// TODO: not yet implemented
+  final Function statePathSelector;
+  /// TODO: not yet implemented
+  final Map extra;
+  const Property(
+      {this.notify: false,
+        this.computed,
+        this.statePath,
+        this.extra,
+        this.statePathSelector});
+}
+
+
+
+/***
+ * Mark a class to become a polymer mixin.
+ * Classes marked with this annotation becomes a js-mixin (a la polymer)
+ * and can be used in `implements` clause like any other polymer mixin.
+ * This functionality replaces the dart mixin feature in a way that is more
+ * js interoperable.
+ */
+class PolymerBehavior {
+  final String name;
+  const PolymerBehavior(this.name);
+}
+
+class Observe {
+  final String observed;
+  const Observe(this.observed);
+}
+
+class Notify {
+  const Notify();
+}
+
+const Notify notify = const Notify();
+
--- lib/polymer_element.dart
+++ lib/polymer_element.dart
@@ -6,9 +6,13 @@ import 'dart:js';
 import 'package:js/js.dart';
 import 'package:js/js_util.dart';
 export 'super.dart' show callSuper;
+export 'package:polymer_element/annotations.dart';
+import 'package:polymer_element/annotations.dart';
 // ignore: UNUSED_IMPORT
 import 'package:polymer_element/polymerize_js.dart';
+// ignore: UNUSED_IMPORT
 import 'metadata_registry.dart';
+import 'package:polymerize_common/init.dart';
 export 'metadata_registry.dart';
 
 const String POLYMER_VERSION = "v2.0.0";
@@ -20,44 +24,7 @@ class Config {
   Config({this.observers});
 }
 
-class PolymerRegister {
-  final String tagName;
-  final String template;
-  final bool native;
-  final List<Type> uses;
-  const PolymerRegister(this.tagName,
-      {this.template, this.native: false, this.uses});
-}
 
-/***
- * Mark a class to become a polymer mixin.
- * Classes marked with this annotation becomes a js-mixin (a la polymer)
- * and can be used in `implements` clause like any other polymer mixin.
- * This functionality replaces the dart mixin feature in a way that is more
- * js interoperable.
- */
-class PolymerBehavior {
-  final String name;
-  const PolymerBehavior(this.name);
-}
-
-class Observe {
-  final String observed;
-  const Observe(this.observed);
-}
-
-class Notify {
-  const Notify();
-}
-
-const Notify notify = const Notify();
-
-class BowerImport {
-  final String ref;
-  final String import;
-  final String name;
-  const BowerImport({this.ref, this.import, this.name});
-}
 
 const _Undefined = const {};
 
@@ -236,24 +203,4 @@ class PropertyEffectsUtils {
 
   static push(PropertyEffects el, String path, [items]) =>
       callMethod(el as dynamic, 'push', [path]..addAll(items));
-}
-
-/**
- * Optional property annotation in order to add metadata info.
- */
-class Property {
-  final bool notify;
-  /// TODO: not yet implemented
-  final String computed;
-  final String statePath;
-  /// TODO: not yet implemented
-  final Function statePathSelector;
-  /// TODO: not yet implemented
-  final Map extra;
-  const Property(
-      {this.notify: false,
-      this.computed,
-      this.statePath,
-      this.extra,
-      this.statePathSelector});
 }
